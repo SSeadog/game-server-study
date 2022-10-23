6f42b1c1db240172ec9dce3b028701e0b530a10d
SSeadog <aasssa1225@naver.com>
2022-10-23 11:38:10 +0900

    패킷직렬화2

--- DummyClient/ServerSession.cs
+++ DummyClient/ServerSession.cs
@@ -6,23 +6,71 @@ using System.Text;
 namespace DummyClient
 {
     // 패킷의 헤더 정보로 보면 됨
-    class Packet
+    abstract class Packet
     {
         public ushort size;
         public ushort packetId;
+
+        public abstract ArraySegment<byte> Write();
+        public abstract void Read(ArraySegment<byte> s);
     }
 
     // 플레이어 인포 요청할 때 사용할 패킷
     class PlayerInfoReq : Packet
     {
         public long playerId;
-    }
 
-    // 플레이어 인포 받을 때 사용할 패킷
-    class PlayerInfoOk : Packet
-    {
-        public int hp;
-        public int attack;
+        public PlayerInfoReq()
+        {
+            this.packetId = (ushort)PacketID.PlayerInfoReq;
+        }
+
+        public override void Read(ArraySegment<byte> s)
+        {
+            ushort count = 0;
+
+            //ushort size = BitConverter.ToUInt16(s.Array, s.Offset);
+            count += 2;
+            //ushort id = BitConverter.ToUInt16(s.Array, s.Offset + count);
+            count += 2;
+            BitConverter.ToUInt64(new ReadOnlySpan<byte>(s.Array, s.Offset + count, s.Count - count));
+            count += 8;
+        }
+
+        public override ArraySegment<byte> Write()
+        {
+            ArraySegment<byte> s = SendBufferHelper.Open(4096);
+
+            ushort count = 0;
+            bool success = true;
+
+            // 효율을 위해 GetBytes 대신 TryWriteBytes 사용
+            // count를 먼저 더한 이유는 TyrWriteBytes에서 해당 비트만큼 쓸거니까 계산하기 위해 먼저 더하기?
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), this.packetId);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), this.playerId);
+            count += 8;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, count), (ushort)4);
+
+            //byte[] size = BitConverter.GetBytes(packet.size); // 2
+            //byte[] packetId = BitConverter.GetBytes(packet.packetId); // 2
+            //byte[] playerId = BitConverter.GetBytes(packet.playerId); // 8
+
+            //Array.Copy(size, 0, s.Array, s.Offset + count, 2);
+            //count += 2;
+            //Array.Copy(packetId, 0, s.Array, s.Offset + count, 2);
+            //count += 2;
+            //Array.Copy(playerId, 0, s.Array, s.Offset + count, 8);
+            //count += 8;
+
+            if (success == false)
+            {
+                return null;
+            }
+
+            return SendBufferHelper.Close(count);
+        }
     }
 
     public enum PacketID
@@ -40,40 +88,14 @@ namespace DummyClient
         {
             Console.WriteLine($"Onconnected: {endPoint}");
 
-            PlayerInfoReq packet = new PlayerInfoReq() { packetId = (ushort)PacketID.PlayerInfoReq, playerId = 1001 };
+            PlayerInfoReq packet = new PlayerInfoReq() { playerId = 1001 };
 
             // 보낸다
             //for (int i = 0; i < 5; i++)
             {
-                ArraySegment<byte> s = SendBufferHelper.Open(4096);
-
-                ushort count = 0;
-                bool success = true;
-
-                // 효율을 위해 GetBytes 대신 TryWriteBytes 사용
-                // count를 먼저 더한 이유는 TyrWriteBytes에서 해당 비트만큼 쓸거니까 계산하기 위해 먼저 더하기?
-                count += 2;
-                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), packet.packetId);
-                count += 2;
-                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), packet.playerId);
-                count += 8;
-                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, count), count);
-
-                //byte[] size = BitConverter.GetBytes(packet.size); // 2
-                //byte[] packetId = BitConverter.GetBytes(packet.packetId); // 2
-                //byte[] playerId = BitConverter.GetBytes(packet.playerId); // 8
-
-                //Array.Copy(size, 0, s.Array, s.Offset + count, 2);
-                //count += 2;
-                //Array.Copy(packetId, 0, s.Array, s.Offset + count, 2);
-                //count += 2;
-                //Array.Copy(playerId, 0, s.Array, s.Offset + count, 8);
-                //count += 8;
-
-                ArraySegment<byte> sendBuff = SendBufferHelper.Close(count);
-
-                if (success)
-                    Send(sendBuff);
+                ArraySegment<byte> s = packet.Write();
+                if (s != null)
+                    Send(s);
             }
         }
 
--- Server/ClientSession.cs
+++ Server/ClientSession.cs
@@ -5,23 +5,71 @@ using System.Net;
 namespace Server
 {
     // 패킷의 헤더 정보로 보면 됨
-    class Packet
+    abstract class Packet
     {
         public ushort size;
         public ushort packetId;
+
+        public abstract ArraySegment<byte> Write();
+        public abstract void Read(ArraySegment<byte> s);
     }
 
     // 플레이어 인포 요청할 때 사용할 패킷
     class PlayerInfoReq : Packet
     {
         public long playerId;
-    }
 
-    // 플레이어 인포 받을 때 사용할 패킷
-    class PlayerInfoOk : Packet
-    {
-        public int hp;
-        public int attack;
+        public PlayerInfoReq()
+        {
+            this.packetId = (ushort)PacketID.PlayerInfoReq;
+        }
+
+        public override void Read(ArraySegment<byte> s)
+        {
+            ushort count = 0;
+
+            //ushort size = BitConverter.ToUInt16(s.Array, s.Offset);
+            count += 2;
+            //ushort id = BitConverter.ToUInt16(s.Array, s.Offset + count);
+            count += 2;
+            BitConverter.ToUInt64(new ReadOnlySpan<byte>(s.Array, s.Offset + count, s.Count - count));
+            count += 8;
+        }
+
+        public override ArraySegment<byte> Write()
+        {
+            ArraySegment<byte> s = SendBufferHelper.Open(4096);
+
+            ushort count = 0;
+            bool success = true;
+
+            // 효율을 위해 GetBytes 대신 TryWriteBytes 사용
+            // count를 먼저 더한 이유는 TyrWriteBytes에서 해당 비트만큼 쓸거니까 계산하기 위해 먼저 더하기?
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), this.packetId);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), this.playerId);
+            count += 8;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, count), count);
+
+            //byte[] size = BitConverter.GetBytes(packet.size); // 2
+            //byte[] packetId = BitConverter.GetBytes(packet.packetId); // 2
+            //byte[] playerId = BitConverter.GetBytes(packet.playerId); // 8
+
+            //Array.Copy(size, 0, s.Array, s.Offset + count, 2);
+            //count += 2;
+            //Array.Copy(packetId, 0, s.Array, s.Offset + count, 2);
+            //count += 2;
+            //Array.Copy(playerId, 0, s.Array, s.Offset + count, 8);
+            //count += 8;
+
+            if (success == false)
+            {
+                return null;
+            }
+
+            return SendBufferHelper.Close(count);
+        }
     }
 
     public enum PacketID
@@ -71,9 +119,9 @@ namespace Server
             {
                 case PacketID.PlayerInfoReq:
                     {
-                        long playerId = BitConverter.ToInt64(buffer.Array, buffer.Offset + count);
-                        count += 8;
-                        Console.WriteLine($"PlauyerInfoReq: {playerId}");
+                        PlayerInfoReq p = new PlayerInfoReq();
+                        p.Read(buffer);
+                        Console.WriteLine($"PlauyerInfoReq: {p.playerId}");
                     }
                     break;
                 case PacketID.PlayerInfoOk:
