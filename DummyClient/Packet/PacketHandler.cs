ec270d6acf117cde5f4a90c4839f3556bcde2c4c
SSeadog <aasssa1225@naver.com>
2022-10-25 01:03:59 +0900

    Packet Generator 완료

---
+++ DummyClient/Packet/PacketHandler.cs
@@ -0,0 +1,14 @@
+﻿using ServerCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class PacketHandler
+{
+    // {패킷이름}Handler 요청하면 됩니다 라고 약속
+    public static void S_TestHandler(PacketSession session, IPacket packet)
+    {
+
+    }
+}
---
+++ Server/Packet/PacketHandler.cs
@@ -0,0 +1,27 @@
+﻿using ServerCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class PacketHandler
+{
+    // {패킷이름}Handler 요청하면 됩니다 라고 약속
+    public static void C_PlayerInfoReqHandler(PacketSession session, IPacket packet)
+    {
+        C_PlayerInfoReq p = packet as C_PlayerInfoReq;
+
+        Console.WriteLine($"PlauyerInfoReq: {p.playerId} {p.name}");
+
+        foreach (C_PlayerInfoReq.Skill skill in p.skills)
+        {
+            Console.WriteLine($"Skill({skill.id})({skill.level})({skill.duration})");
+        }
+    }
+
+    public static void TestHandler(PacketSession session, IPacket packet)
+    {
+        
+    }
+}
