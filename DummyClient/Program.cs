15b3f07a82009cdee81ce9df89cff7e505b41544
SSeadog <aasssa1225@naver.com>
2022-10-24 15:37:07 +0900

    Squashed commit of the following:
    commit df1184143c2527ea25143e20efa8e7f0b6f910c8
    Author: SSeadog <aasssa1225@naver.com>
    Date:   Sun Oct 23 18:09:22 2022 +0900
    
        패킷 직렬화4까지 완료(string, array 직렬화 추가)
    
    commit 6f42b1c1db240172ec9dce3b028701e0b530a10d
    Author: SSeadog <aasssa1225@naver.com>
    Date:   Sun Oct 23 11:38:10 2022 +0900
    
        패킷직렬화2
    
    commit 181459c2a965ca8e6801768b918104aeb6936ec6
    Author: SSeadog <aasssa1225@naver.com>
    Date:   Sun Oct 23 10:37:53 2022 +0900
    
        패킷직려화1 오류 수정
    
    commit c30d7f8ecdaba711e62d13faddf5fe9182040429
    Author: SSeadog <aasssa1225@naver.com>
    Date:   Sun Oct 23 10:15:37 2022 +0900
    
        패킷직렬화1

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -4,56 +4,7 @@ using System.Net.Sockets;
 using System.Text;
 
 namespace DummyClient
-{
-    class Packet
-    {
-        public ushort size;
-        public ushort packetId;
-    }
-
-    class GameSession : Session
-    {
-        public override void OnConnected(EndPoint endPoint)
-        {
-            Console.WriteLine($"Onconnected: {endPoint}");
-
-            Packet packet = new Packet() { size = 4, packetId = 7 };
-
-            // 보낸다
-            for (int i = 0; i < 5; i++)
-            {
-                ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
-                byte[] buffer = BitConverter.GetBytes(packet.size);
-                byte[] buffer2 = BitConverter.GetBytes(packet.packetId);
-                Array.Copy(buffer, 0, openSegment.Array, openSegment.Offset, buffer.Length);
-                Array.Copy(buffer2, 0, openSegment.Array, openSegment.Offset + buffer.Length, buffer2.Length);
-                ArraySegment<byte> sendBuff = SendBufferHelper.Close(buffer.Length + buffer2.Length);
-
-                Send(sendBuff);
-            }
-        }
-
-        public override void OnDisconnected(EndPoint endPoint)
-        {
-            Console.WriteLine($"OnDisconnected: {endPoint}");
-        }
-
-        // 패킷을 받으면 어떻게 처리하겠다는 규약을 정해야함
-        // 패킷은 일부만 받으면 수행x
-        // 이동 패킷 (특정 좌표로 이동하고 싶다)
-        // 15 3 2
-        public override int OnRecv(ArraySegment<byte> buffer)
-        {
-            string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
-            Console.WriteLine($"[From Server] {recvData}");
-            return buffer.Count;
-        }
-
-        public override void OnSend(int numOfBytes)
-        {
-            Console.WriteLine($"Transferred bytes: {numOfBytes}");
-        }
-    }
+{ 
     class Program
     {
         static void Main(string[] args)
@@ -66,7 +17,7 @@ namespace DummyClient
 
             Connector connector = new Connector();
 
-            connector.Connect(endPoint, () => { return new GameSession(); });
+            connector.Connect(endPoint, () => { return new ServerSession(); });
 
             while (true)
             {
---
+++ Server/ClientSession.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using ServerCore;
+using System.Net;
+using System.Text;
+
+namespace Server
+{
+    // 패킷의 헤더 정보로 보면 됨
+    abstract class Packet
+    {
+        public ushort size;
+        public ushort packetId;
+
+        public abstract ArraySegment<byte> Write();
+        public abstract void Read(ArraySegment<byte> s);
+    }
+
+    // 플레이어 인포 요청할 때 사용할 패킷
+    class PlayerInfoReq : Packet
+    {
+        public long playerId;
+        public string name;
+
+        public struct SkillInfo
+        {
+            public int id;
+            public short level;
+            public float duration;
+
+            public bool Write(Span<byte> s, ref ushort count)
+            {
+                bool success = true;
+                success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), id);
+                count += sizeof(int);
+                success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), level);
+                count += sizeof(short);
+                success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), duration);
+                count += sizeof(float);
+
+                return success;
+            }
+
+            public void Read(ReadOnlySpan<byte> s, ref ushort count)
+            {
+                id = BitConverter.ToInt32(s.Slice(count, s.Length - count));
+                count += sizeof(int);
+                level = BitConverter.ToInt16(s.Slice(count, s.Length - count));
+                count += sizeof(short);
+                duration = BitConverter.ToSingle(s.Slice(count, s.Length - count));
+                count += sizeof(float);
+            }
+        }
+
+        public List<SkillInfo> skills = new List<SkillInfo>();
+
+        public PlayerInfoReq()
+        {
+            this.packetId = (ushort)PacketID.PlayerInfoReq;
+        }
+
+        public override void Read(ArraySegment<byte> segment)
+        {
+            ushort count = 0;
+
+            ReadOnlySpan<byte> s = new ReadOnlySpan<byte>(segment.Array, segment.Offset + count, segment.Count - count);
+
+            count += sizeof(ushort);
+            count += sizeof(ushort);
+            this.playerId = BitConverter.ToInt64(s.Slice(count, s.Length - count));
+            count += sizeof(long);
+
+            // string
+            ushort nameLen = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
+            count += sizeof(ushort);
+            this.name = Encoding.Unicode.GetString(s.Slice(count, nameLen));
+            count += nameLen;
+
+            // skill list
+            skills.Clear();
+            ushort skillLen = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
+            count += sizeof(ushort);
+            for (int i = 0; i < skillLen; i++)
+            {
+                SkillInfo skill = new SkillInfo();
+                skill.Read(s, ref count);
+                skills.Add(skill);
+            }
+        }
+
+        public override ArraySegment<byte> Write()
+        {
+            ArraySegment<byte> segment = SendBufferHelper.Open(4096);
+
+            ushort count = 0;
+            bool success = true;
+
+            Span<byte> s = new Span<byte>(segment.Array, segment.Offset, segment.Count);
+
+            // 효율을 위해 GetBytes 대신 TryWriteBytes 사용
+            // count를 먼저 더한 이유는 TyrWriteBytes에서 해당 비트만큼 쓸거니까 계산하기 위해 먼저 더하기?
+            count += sizeof(ushort);
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), this.packetId);
+            count += sizeof(ushort);
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), this.playerId);
+            count += sizeof(long);
+
+            //byte[] size = BitConverter.GetBytes(packet.size); // 2
+            //byte[] packetId = BitConverter.GetBytes(packet.packetId); // 2
+            //byte[] playerId = BitConverter.GetBytes(packet.playerId); // 8
+
+            //Array.Copy(size, 0, s.Array, s.Offset + count, 2);
+            //count += 2;
+            //Array.Copy(packetId, 0, s.Array, s.Offset + count, 2);
+            //count += 2;
+            //Array.Copy(playerId, 0, s.Array, s.Offset + count, 8);
+            //count += 8;
+
+            // string
+            // string len [2] -> byte []
+            //ushort nameLen = (ushort)Encoding.Unicode.GetByteCount(this.name);
+            //success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), nameLen);
+            //count += sizeof(ushort);
+            //Array.Copy(Encoding.Unicode.GetBytes(this.name), 0, segment.Array, count, nameLen);
+            //count += nameLen;
+
+            // string
+            ushort nameLen = (ushort)Encoding.Unicode.GetBytes(this.name, 0, this.name.Length, segment.Array, segment.Offset + count + sizeof(ushort));
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), nameLen);
+            count += sizeof(ushort);
+            count += nameLen;
+
+            // skill list
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), (ushort)skills.Count);
+            count += sizeof(ushort);
+            foreach (SkillInfo skill in skills)
+            {
+                success &= skill.Write(s, ref count);
+            }
+
+            success &= BitConverter.TryWriteBytes(s, count);
+
+            if (success == false)
+            {
+                return null;
+            }
+
+            return SendBufferHelper.Close(count);
+        }
+    }
+
+    public enum PacketID
+    {
+        PlayerInfoReq = 1,
+        PlayerInfoOk = 2,
+    }
+
+    class ClientSession : PacketSession
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnCconnected: {endPoint}");
+
+            //Packet packet = new Packet() { size = 100, packetId = 10 };
+
+            // 이 부분은 자동화해줄 거라 걱정 안해도 됨
+            // 데이터를 Buffer에 밀어넣는 작ㅖ 
+            //ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
+            //byte[] buffer = BitConverter.GetBytes(packet.size);
+            //byte[] buffer2 = BitConverter.GetBytes(packet.packetId);
+            //Array.Copy(buffer, 0, openSegment.Array, openSegment.Offset, buffer.Length);
+            //Array.Copy(buffer2, 0, openSegment.Array, openSegment.Offset + buffer.Length, buffer2.Length);
+            //ArraySegment<byte> sendBuff = SendBufferHelper.Close(buffer.Length + buffer2.Length);
+
+            //byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to Server!");
+            //Send(sendBuff);
+            Thread.Sleep(5000);
+            Disconnect();
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected: {endPoint}");
+        }
+
+        public override void OnRecvPacket(ArraySegment<byte> buffer)
+        {
+            ushort count = 0;
+
+            ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
+            count += 2;
+            ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
+            count += 2;
+
+            switch ((PacketID)id)
+            {
+                case PacketID.PlayerInfoReq:
+                    {
+                        PlayerInfoReq p = new PlayerInfoReq();
+                        p.Read(buffer);
+                        Console.WriteLine($"PlauyerInfoReq: {p.playerId} {p.name}");
+
+                        foreach (PlayerInfoReq.SkillInfo skill in p.skills)
+                        {
+                            Console.WriteLine($"Skill({skill.id})({skill.level})({skill.duration})");
+                        }
+                    }
+                    break;
+                case PacketID.PlayerInfoOk:
+                    break;
+            }
+
+            Console.WriteLine($"RecvPacketId: {id}, Size: {size}");
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine($"Transferred bytes: {numOfBytes}");
+        }
+    }
+}
+
